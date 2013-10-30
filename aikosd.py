70010277df6f459dd3c81edc76947cca59cd2342
Chris Cummins <chrisc.101@gmail.com>
2013-10-30 12:48:02 +0000

    Add is_process_owned_by_init() test
    Issue #2.

--- aikosd.py
+++ aikosd.py
@@ -4,11 +4,18 @@ import os
 import sys
 import time
 
+# The process ID of init
+INIT_PID = 1
+
 def debug(msg):
 	f = open("debug.log", "a")
 	f.write("[" + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()) + "] "
 			+ msg + "\n")
 	f.close()
+
+# Returns whether the process is owned by init
+def is_process_owned_by_init():
+	return os.getppid() == INIT_PID
 
 # Fork twice and kill the first two processes to create an orphan process which
 # will get reassigned to init.
@@ -18,6 +25,7 @@ def fork_and_orphan():
 	if pid == 0:
 		pid = os.fork()
 		if pid == 0:
+			assert is_process_owned_by_init() == True
 			debug("Finished")
 	sys.exit(0)
 
