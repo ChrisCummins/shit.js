abb56d797a8e4b77c10fde752976315bee8e82d3
Chris Cummins <chrisc.101@gmail.com>
2013-11-10 22:03:26 +0000

    Listen on port

--- aikosd.js
+++ aikosd.js
@@ -3,3 +3,8 @@ var config = require('./config').values
 var app = require('./app').getApp(config)
 
 var port = parseInt(process.argv[2]) || 8080
+
+app.listen(port);
+
+console.log('Server listening on port %d in %s mode',
+            app.address().port, app.settings.env);
