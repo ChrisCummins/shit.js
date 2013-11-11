c4e6a018652a3a9b4fe33b66ce098e017fa4494b
Chris Cummins <chrisc.101@gmail.com>
2013-11-11 12:40:47 +0000

    filewatcher.js: Abstract callback functions

--- filewatcher.js
+++ filewatcher.js
@@ -2,28 +2,36 @@ var watchr = require('watchr');
 
 var FileWatcher = function(path, args) {
 
+  function logListener(logLevel) {
+    console.log('a log message occured:', arguments);
+  };
+
+  function errorListener(error) {
+    console.log('an error occured:', error);
+  };
+
+  function watchingListener(error, watcherInstance, isWatching) {
+    if (error) {
+      console.log("watching the path " + watcherInstance.path + " failed with error", error);
+    } else {
+      console.log("watching the path " + watcherInstance.path + " completed");
+    }
+  };
+
+  function changeListener(changeType, filePath,
+                          currentStat, previousStat) {
+    console.log('a change event occured:',arguments);
+  };
+
   watchr.watch({
     path: path,
     persistent: true,
     duplicateDelay: 2000,
     listeners: {
-      log: function(logLevel) {
-        // console.log('a log message occured:', arguments);
-      },
-      error: function(error) {
-        console.log('an error occured:', error);
-      },
-      watching: function(error, watcherInstance, isWatching) {
-        if (error) {
-          console.log("watching the path " + watcherInstance.path + " failed with error", error);
-        } else {
-          console.log("watching the path " + watcherInstance.path + " completed");
-        }
-      },
-      change: function(changeType, filePath,
-                       currentStat, previousStat) {
-        console.log('a change event occured:',arguments);
-      }
+      log: logListener,
+      error: errorListener,
+      watching: watchingListener,
+      change: changeListener,
     },
     next: function(error, watchers) {
       if (error) {
