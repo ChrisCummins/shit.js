8c801edf5a76647f129c9e9adec4f899e1d60582
Chris Cummins <chrisc.101@gmail.com>
2013-11-11 00:10:54 +0000

    Add primitive filesystem watcher

--- watch.js
+++ watch.js
@@ -1,5 +1,6 @@
 var config = require('./config').values;
 var util = require('./util');
+var watchr = require('watchr');
 
 exports.createWatch = function(server) {
 
@@ -18,20 +19,44 @@ exports.createWatch = function(server) {
     };
   };
 
-  function debug_tick() {
-
-    function pushMessage(msg) {
-      messages.push(msg);
-
-      return messages;
-    };
-
-    pushMessage({ type: "Debug", message: new Date() });
+  function pushMessage(type, msg, path) {
+    messages.push({ type: type, message: msg, path: path });
 
     broadcast(sessions, 'messages', messages);
   };
 
-  setInterval(debug_tick, 1000);
+  watchr.watch({
+    paths: ['test'],
+    listeners: {
+      log: function(logLevel) {
+        var array = Array.prototype.slice.call(arguments, 0);
+
+        pushMessage('Debug', array, '');
+      },
+      error: function(error) {
+        pushMessage('Error', error, '');
+      },
+      watching: function(error, watcherInstance, isWatching) {
+        if (err) {
+          pushMessage('Error', error, watcherInstance.path);
+        } else {
+          pushMessage("Log", 'Watching completed', watcherInstance.path);
+        }
+      },
+      change: function(changeType, filePath,
+                       fileCurrentStat, filePreviousStat) {
+        pushMessage('Change', changeType + ', ' + fileCurrentState +
+                    ', ' + filePreviousStat, filePath);
+      }
+    },
+    next: function(error, watchers) {
+      if (error) {
+        pushMessage('Error', error, '');
+      } else {
+        pushMessage('Log', 'Watching completed', '');
+      }
+    }
+  });
 
   socket.on('connection', function(client) {
 
@@ -48,6 +73,8 @@ exports.createWatch = function(server) {
     client.on('join', function (data) {
       util.add(sessions, client.id);
       clients[client.id] = client;
+
+      broadcast(sessions, 'messages', messages);
     });
 
   });
