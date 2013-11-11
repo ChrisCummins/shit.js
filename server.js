dc572500ec1db4f2831a320647344d80efff7834
Chris Cummins <chrisc.101@gmail.com>
2013-11-11 15:27:27 +0000

    server.js: Minor style fixes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var config = require('./config').values;
 var App = require('./app');
 var Aikos = require('./aikos');
 
 var port = parseInt(process.argv[2]) || 8080;
+
+var config = require('./config').values;
 
 var app = new App(config).listen(port);
 var aikos = new Aikos(app);
@@ -11,7 +12,7 @@ console.log('Server listening on port %d in %s mode.',
             app.address().port, app.settings.env);
 
 process.on('SIGINT', function() {
-	app.close();
-	console.log('\nShutting down server.');
-	process.exit(0);
+  app.close();
+  console.log('\nShutting down server.');
+  process.exit(0);
 })
